f9a1b9bac754e6bf1cb5fb333179cdbe3f9f5146
Balint Laczko <balintlaczko01@gmail.com>
2019-04-29 16:11:38 +0200

    deleted unused functions, and restructured code (helper functions first, interface functions at the bottom)

--- javascript/SceneManager.js
+++ javascript/SceneManager.js
@@ -17,22 +17,65 @@ var scenes = new Dict("VTM_scenes");
 var coreCanvas = this.patcher.parentpatcher.parentpatcher.getnamed("sceneCores").subpatcher().getnamed("coreCanvas");
 var coreCanvasPatcher = coreCanvas.subpatcher();
 
-function sceneExists(sceneName) {
-	var result = false;
-	var names = scenes.getkeys();
-	for(i = 0; i < names.length; i = i + 1) {
-		if(names[i] == sceneName) {
-			result = true;
-			break;
-		}
-	};
-	return result;
-}
-
+//used in: newScene
 function escapeSpaces( str ) {
 	var splitStr = str.split(" ");
 	var newStr = splitStr.join("\\\\ ");
 	return newStr;
+}
+
+//used in: deleteSceneCore, makeSceneCore
+function sceneCoreExists(sceneName) {
+	var result = false;
+	var obj = coreCanvasPatcher.firstobject;
+	while(obj != null) {
+		if(obj.varname == sceneName) {
+			result = true;
+			break;
+		}
+		obj = obj.nextobject;
+	}
+	return result;
+}
+
+//used in: registerClosedScene
+function deleteSceneCore( sceneName ) {
+	if(sceneCoreExists( sceneName ) ) {
+		coreCanvasPatcher.remove(
+			coreCanvasPatcher.getnamed(sceneName)
+		);
+	}
+}
+
+//used in: registerOpenedScene
+//if the core already exists it does nothing
+function makeSceneCore( sceneName ) {
+	var newobj;
+	post("Making scene core for " + sceneName + "\n");
+	if(!sceneCoreExists( sceneName ) ) {
+		newobj = coreCanvasPatcher.newdefault(
+			0, 0,
+			"SceneCore.model",
+			sceneName
+		);
+		newobj.varname = sceneName;
+	} else {
+		post("Scene core '" + sceneName + "' already exists\n");
+	}
+}
+
+function registerOpenedScene(sceneName) {
+	scenes.setparse(
+		sceneName,
+		'{"mapped": 0, "active": 0}'
+	);
+	makeSceneCore(sceneName);
+	//post("Making scene core for " + sceneName + "\n");
+}
+
+function registerClosedScene(sceneName) {
+	scenes.remove(sceneName);
+	deleteSceneCore(sceneName);
 }
 
 function newScene( scriptFolder, scenePath ) {
@@ -48,112 +91,4 @@ function newScene( scriptFolder, scenePath ) {
 		"cd " + escapeSpaces(scriptFolder) + "\\; ./makeNewScene " + scenePath
 	);
 	//I hate Max.
-}
-
-
-//assumes the the scene exists
-//return false if not.
-function getBooleanValueForScene(sceneName, key) {
-	var result = false;
-	var scene;
-	scene = scenes.get(sceneName);
-	if(scene != null) {
-		result = scene.get(key);
-		if(result == 1) {
-			result = true;
-		} else {
-			result = false;
-		}
-	}
-	return result;
-}
-
-function sceneIsOpen(sceneName) {
-	return getBooleanValueForScene(sceneName, "open");
-}
-
-function sceneIsActive(sceneName) {
-	return getBooleanValueForScene(sceneName, "active");
-}
-
-function sceneIsMapped(sceneName) {
-	return getBooleanValueForScene(sceneName, "mapped");
-}
-
-function openScene(sceneName) {
-	// does the scene exist?
-	if(sceneExists(sceneName)) {
-		post("opening scene: " + sceneName); post();
-		if(sceneIsOpen(sceneName)) {
-			post("Scene already open: " + sceneName); post();
-		} else {
-			makeSceneCore( sceneName );
-			makeSceneView( sceneName );
-			showSceneView( sceneName );
-		}
-	} else {
-		post("scene does not exist: " + sceneName); post();
-	}
-}
-
-function closeScene( sceneName ) {
-	// does the scene exist?
-	if(sceneExists(sceneName)) {
-		deleteSceneCore( sceneName );
-		hideSceneView( sceneName );
-		deleteSceneView( sceneName );
-	} else {
-		post("scene does not exist: " + sceneName); post();
-	}
-}
-
-function sceneCoreExists(sceneName) {
-	var result = false;
-	var obj = coreCanvasPatcher.firstobject;
-	while(obj != null) {
-		if(obj.varname == sceneName) {
-			result = true;
-			break;
-		}
-		obj = obj.nextobject;
-	}
-	return result;
-}
-
-function deleteSceneCore( sceneName ) {
-	if(sceneCoreExists( sceneName ) ) {
-		coreCanvasPatcher.remove(
-			coreCanvasPatcher.getnamed(sceneName)
-		);
-	}
-}
-
-function registerOpenedScene(sceneName) {
-	scenes.setparse(
-		sceneName,
-		'{"mapped": 0, "active": 0}'
-	);
-	makeSceneCore(sceneName);
-	post("Making scene core for " + sceneName + "\n");
-}
-
-function registerClosedScene(sceneName) {
-	scenes.remove(sceneName);
-	deleteSceneCore(sceneName);
-}
-
-//if the core already exists it does nothing
-function makeSceneCore( sceneName ) {
-	var newobj;
-	post("Making scene core for " + sceneName + "\n");
-	if(!sceneCoreExists( sceneName ) ) {
-		newobj = coreCanvasPatcher.newdefault(
-			0, 0,
-			"SceneCore.model",
-			sceneName
-		);
-		newobj.varname = sceneName;
-	} else {
-		post("Scene core '" + sceneName + "' already exists\n");
-	}
 }
